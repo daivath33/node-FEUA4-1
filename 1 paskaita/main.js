24f76680c22110c6b828bae5507629d9b9dd670d
daiva <daivath33@gmail.com>
2023-04-13 19:25:45 +0300

    created first node project and gitignore file

--- 1 paskaita/main.js
+++ 1 paskaita/main.js
@@ -1,1 +1,8 @@
-console.log("Node JS testas");
+const casual = require("casual");
+
+let city = casual.city;
+//console.log(city, Math.floor(Math.random() * 10) + 1);
+//console.log(casual.country);
+console.log(casual.integer((from = 10), (to = 100)));
+const name = `${casual.name_prefix} ${casual.first_name} ${casual.last_name}`;
+console.log(name);
