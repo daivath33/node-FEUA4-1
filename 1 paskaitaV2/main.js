689d10c166ef6121fbaf9664245164f3ca568a6b
daiva <daivath33@gmail.com>
2023-04-13 20:00:29 +0300

    created two routas

---
+++ 1 paskaitaV2/main.js
@@ -0,0 +1,23 @@
+const express = require("express"); //importavimas Express modulio
+
+const app = express(); //aplikacijos sukurimas
+const port = 3000; //porto (kanalo) skaicius
+
+//routas (kelias) route/path
+//get = grazink duomenis
+//http://localhost:3000/
+app.get("/", (req, res) => {
+  //req - request, tai kas ateina is isores
+  //res - response, tai kas ateina is vidaus
+  //send metodas issiuncia duomenis
+  res.send("Hello NODE_JS!!");
+});
+
+app.get("/today", (req, res) => {
+  res.send(new Date().toDateString());
+});
+
+//Serverio paleidimas
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
