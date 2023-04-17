f676d8031e86869e2dd7b39d03775a2ec6248770
daiva <daivath33@gmail.com>
2023-04-17 21:39:20 +0300

    prudetas pavadinimas

--- 2 paskaita_2uzduotis/main.js
+++ 2 paskaita_2uzduotis/main.js
@@ -7,6 +7,10 @@ app.use(express.json());
 
 const products = [];
 
+app.get("/", (req, res) => {
+  res.send("2 Paskaita 2 Uzduotis");
+});
+
 app.get("/products", (req, res) => {
   res.send(products);
 });
@@ -16,5 +20,6 @@ app.post("/products", (req, res) => {
   res.send(body);
   products.push(body);
 });
+
 const port = 3000;
 app.listen(port, () => console.log(`Server started on port ${port}...`));
